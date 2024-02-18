56b0530ec2fc05f06adc3ccfc311efe7f3aff683
Your Name <nusratriya887@gmail.com>
2024-02-18 13:24:38 +0600

    new Code

--- src/components/HelpDesk.jsx
+++ src/components/HelpDesk.jsx
@@ -8,7 +8,7 @@ export default function HelpDesk() {
 <a className='text-lg font-semibold cursor-pointer text-gray-600 flex'>Help Center</a>
 
 <span className='helpDeskText flex text-slate-700  justify-between gap-5'>
-    <a className='text-orange-600 border-orange-600 border-y-2  ToolsBorder' href="">Home Page</a>
+    <a className='text-orange-600 border-b-orange-600 border-b-2  pb-4' href="">Home Page</a>
     <a className='hover:text-orange-500' href="">FAQ</a>
 </span>
 
--- src/components/SelfServices.jsx
+++ src/components/SelfServices.jsx
@@ -3,11 +3,52 @@ import React from 'react'
 export default function SelfServices() {
   return (
    <>
+    <div>
     <div className='mt-5 items-center justify-center flex '>
         
-        <p className='text-sm border-y-2 text-gray-500'>Self Service Tools</p>
-        
+        <p className='text-sm pb-3 border-orange-600 border-b-2  text-gray-500'>Self Service Tools</p>
         </div>
+        <div>
+<div className='flex items-center justify-around mt-5'>
+<span className='bg-blue-500'>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+</div>
+<div>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+<span>
+  <h1>logo</h1>
+  <h1>Track My Order</h1>
+</span>
+</div>
+
+
+        </div>
+    </div>
    </>
   )
 }
