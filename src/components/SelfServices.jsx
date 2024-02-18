4cc845580d2dd293b539ee30b6e213dca68e0fb9
Your Name <nusratriya887@gmail.com>
2024-02-18 14:18:17 +0600

    new Codes

--- src/components/SelfServices.jsx
+++ src/components/SelfServices.jsx
@@ -6,43 +6,43 @@ export default function SelfServices() {
     <div>
     <div className='mt-5 items-center justify-center flex '>
         
-        <p className='text-sm pb-3 border-orange-600 border-b-2  text-gray-500'>Self Service Tools</p>
+        <p className='pb-3 border-orange-600 border-b-2  text-gray-500'>Self Service Tools</p>
         </div>
-        <div>
-<div className='flex items-center justify-around mt-5'>
-<span className='bg-blue-500'>
+        <div className='sm:m-5'>
+<div className='TextSize flex items-center justify-around mt-5'>
+<span className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
   <h1>logo</h1>
   <h1>Track My Order</h1>
+</span  >
+<span  className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
+  <h1>logo</h1>
+  <h1>My Password Reset</h1>
 </span>
-<span>
+<span  className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
   <h1>logo</h1>
-  <h1>Track My Order</h1>
+  <h1>My Payment Options</h1>
 </span>
-<span>
+<span  className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
   <h1>logo</h1>
-  <h1>Track My Order</h1>
-</span>
-<span>
-  <h1>logo</h1>
-  <h1>Track My Order</h1>
+  <h1>My Profile</h1>
 </span>
 </div>
-<div>
-<span>
+<div className='flex TextSize  items-center justify-around mt-2'>
+<span className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
   <h1>logo</h1>
-  <h1>Track My Order</h1>
+  <h1>My Vouchers</h1>
+</span  >
+<span  className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
+  <h1>logo</h1>
+  <h1>Return My Order</h1>
 </span>
-<span>
+<span  className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
   <h1>logo</h1>
-  <h1>Track My Order</h1>
+  <h1>My Address Book</h1>
 </span>
-<span>
+<span  className='borders h-14 w-24 sm:h-16 sm:w-44 m-0.5 sm:pt-4'>
   <h1>logo</h1>
-  <h1>Track My Order</h1>
-</span>
-<span>
-  <h1>logo</h1>
-  <h1>Track My Order</h1>
+  <h1>Cancel My Order</h1>
 </span>
 </div>
 
