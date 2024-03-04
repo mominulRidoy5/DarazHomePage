5be6d295fe0eb6e44e6ae7fa3cbe59b6140cadaa
Your Name <nusratriya887@gmail.com>
2024-03-04 14:44:47 +0600

    EndSite

--- src/components/EndSite.jsx
+++ src/components/EndSite.jsx
@@ -3,36 +3,38 @@ import React from 'react'
 export default function EndSite() {
   return (
     <>
-    <div className='sm:mt-10 mt-7 mb-2 bg-gray-200 sm:flex items-center justify-around'>
-<div className='sm:w-48'>
-    <div className='m-5'>
+    <div className='sm:mt-10 mt-7 cursor-auto mb-2 bg-gray-200 sm:flex items-center justify-around'>
+<div className='sm:w-52'>
+    <div className='m-5 cursor-pointer'>
         <h1 className='items-start flex text-orange-600'>Customer Care</h1>
-        <p className='helpDeskText flex items-start mt-2'>Help Center</p>
-        <p className='helpDeskText flex items-start'>hello Daraz</p>
-        <p className='helpDeskText flex items-start'>hello Daraz</p>
-        <p className='helpDeskText flex items-start'>hello Daraz</p>
-        <p className='helpDeskText flex items-start'>hello Daraz</p>
-        <p className='helpDeskText flex items-start'>hello Daraz</p>
+        <p className='SubNavText hover:font-semibold hover:underline flex items-start mt-2'>Help Center</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>How To Buy</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Return & Refunds</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Contact Us </p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Terms & Codtidions</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>CCMS - Central Complain <br /> Management System</p>
         
     </div>
-    <div>
-        <h1>hello Daraz</h1>
-        <h1>hello Daraz</h1>
-        <h1>hello Daraz</h1>
-        <h1>hello Daraz</h1>
+    <div className='m-5 cursor-pointer'>
+        <h1 className='items-start flex text-orange-600'>Earn With Daraz</h1>
+        <p className='SubNavText hover:font-semibold hover:underline flex items-start mt-2'>Daraz University</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Sell on Daraz</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Code of Conduct</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Join the Daraz Affliate <br /> Program</p>
+
     </div>
 </div>
-<div className='bg-blue-500 sm:w-48'>
+<div className='bg-blue-500 sm:w-52'>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
 </div>
-<div className='bg-yellow-500 sm:w-48'>
+<div className='bg-yellow-500 sm:w-52'>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
 </div>
-<div className='bg-orange-600 sm:w-48'>
+<div className='bg-orange-600 sm:w-52'>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
