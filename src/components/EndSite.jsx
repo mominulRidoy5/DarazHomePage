22543e718c92992e445193822475b4aea996d00f
Your Name <nusratriya887@gmail.com>
2024-03-07 15:18:39 +0600

    Next code

--- src/components/EndSite.jsx
+++ src/components/EndSite.jsx
@@ -3,38 +3,102 @@ import React from 'react'
 export default function EndSite() {
   return (
     <>
-    <div className='sm:mt-10 mt-7 cursor-auto mb-2 bg-gray-200 sm:flex items-center justify-around'>
-<div className='sm:w-52'>
-    <div className='m-5 cursor-pointer'>
+    <div className='mt-10 p-5 cursor-auto  bg-gray-200 sm:flex '>
+<div className='sm:w-60  sm:h-screen'>
+    <div className='p-2 cursor-pointer '>
         <h1 className='items-start flex text-orange-600'>Customer Care</h1>
         <p className='SubNavText hover:font-semibold hover:underline flex items-start mt-2'>Help Center</p>
         <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>How To Buy</p>
         <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Return & Refunds</p>
         <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Contact Us </p>
         <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Terms & Codtidions</p>
-        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>CCMS - Central Complain <br /> Management System</p>
+        <p className='SubNavText   hover:underline mt-0.5 flex items-start sm:break-before-auto sm:-ms-1'>CCMS - Central Complain  Management System</p>
         
     </div>
-    <div className='m-5 cursor-pointer'>
+    <div className='p-2 cursor-pointer sm:mt-3 '>
         <h1 className='items-start flex text-orange-600'>Earn With Daraz</h1>
-        <p className='SubNavText hover:font-semibold hover:underline flex items-start mt-2'>Daraz University</p>
+        <p className='SubNavText hover:font-semibold hover:underline flex items-start'>Daraz University</p>
         <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Sell on Daraz</p>
         <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Code of Conduct</p>
-        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Join the Daraz Affliate <br /> Program</p>
+        <p className='SubNavText hover:font-semibold  hover:underline mt-0.5 flex items-start sm:break-before-auto sm:-ms-1'>Join the Daraz Affliate
+Program</p>
+     
+    </div>
+</div>
+<div className='sm:w-60 sm:h-screen'>
+    <div className='p-2 cursor-pointer'>
+        <h1 className='items-start flex text-orange-600'>Daraz</h1>
+        <p className='SubNavText hover:font-semibold hover:underline flex items-start mt-2'>About Daraz</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Digital Payments</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Digital Payments</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Digital Payments</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Digital Payments</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Digital Payments</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz Card</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz Blog</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz Cares</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz Mart</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Privacy Policy</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz App</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz Exclusives</p>
+        <p className='SubNavText  hover:font-semibold hover:underline mt-0.5 flex items-start'>Daraz Donates</p>
+        
+    </div>
+
+</div>
+<div className='sm:w-60 sm:h-screen pt-2'>
+    <div className='cursor-pointer '>
+        <h1 className='items-start flex text-orange-600'>Daraz International</h1>
+     <div className='flex items-start gap-5 mt-3'>
+        <span className='flex items-center justify-center gap-1'>
+        <img className='h-6 w-6 rounded-full' src="./src/assets/pakistan.jpeg" alt="hello" />
+            <p className='SubNavText hover:underline'>Pakistan</p>
+        </span>
+        <span className='flex items-center justify-center gap-1'>
+           <img className='h-6 w-6 rounded-full' src="./src/assets/Bangladesh.webp" alt="hello" />
+            <p className='SubNavText hover:underline'>Bangladesh</p>
+        </span>
+     </div>
+     <div className='flex items-start gap-4 mt-2'>
+        <span className='flex items-center justify-center gap-1'>
+        <img className='h-6 w-6 rounded-full' src="./src/assets/srilanka.png" alt="hello" />
+            <p className='SubNavText hover:underline'>Shrilanka</p>
+        </span>
+        <span className='flex items-center justify-center gap-1'>
+            
+            <img className='h-6 w-6 rounded-full' src="./src/assets/myanmar.png" alt="hello" />
+        <p className='SubNavText hover:underline'>Myanmar</p>
+        </span>
+     </div>
+     <div className='flex items-start mt-3'>
+        <span className='flex items-center justify-center gap-1'>
+        <img className='h-6 w-6 rounded-full' src="./src/assets/nepal.png" alt="hello" />
+            <p className='SubNavText hover:underline'>Nepal</p>
+        </span>
+      
+     </div>
+    </div>
+    <div className=' cursor-pointer mt-5'>
+        <h1 className='flex items-start text-orange-600'>Payment Method</h1>
+        <div className='flex  gap-2 mt-2'>
+<img className='h-7 w-12 bg-white' src="./src/assets/cashonDelivery.png" alt="hello" />
+<img className='h-7 w-12 bg-white' src="./src/assets/visa.png" alt="hello" />
+<img className='h-7 w-12 bg-white' src="./src/assets/mastercard.png" alt="hello" />
+<img className='h-7 w-12  bg-white' src="./src/assets/american.png" alt="hello" />
+
+</div>
+<div className='flex  gap-2 mt-2'>
+<img className='h-7 w-12 bg-white' src="./src/assets/installments.png" alt="hello" />
+<img className='h-7 w-12 bg-white' src="./src/assets/bkash.png" alt="hello" />
+<img className='h-7 w-12 bg-white' src="./src/assets/nagad.png" alt="hello" />
+<img className='h-7 w-12' src="./src/assets/duchbangla.jpeg" alt="hello" />
+
+</div>
+<img className='h-7 w-12 mt-2 bg-white' src="./src/assets/rocket.png" alt="hello" />
 
     </div>
 </div>
-<div className='bg-blue-500 sm:w-52'>
-    <h1>hello Daraz</h1>
-    <h1>hello Daraz</h1>
-    <h1>hello Daraz</h1>
-</div>
-<div className='bg-yellow-500 sm:w-52'>
-    <h1>hello Daraz</h1>
-    <h1>hello Daraz</h1>
-    <h1>hello Daraz</h1>
-</div>
-<div className='bg-orange-600 sm:w-52'>
+<div className='sm:h-screen sm:w-60'>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
     <h1>hello Daraz</h1>
